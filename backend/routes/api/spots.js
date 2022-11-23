74cd3b5d91d592666200707874cc7f9d687596b4
paul fixler <fixlerpaul@gmail.com>
2022-11-22 19:41:26 -0500

    get all spots endpoint

---
+++ backend/routes/api/spots.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const { Spot, Review, SpotImage } = require('../../db/models');
+const router = express.Router();
+
+
+// get all the spots with avg rating and preview img
+router.get('/', async (req, res) => {
+    const spots = await Spot.findAll({
+        include: [
+            {
+                model: Review
+            },
+            {
+                model: SpotImage
+            }
+        ]
+
+    })
+
+    let spotsList = [];
+    spots.forEach(spot => {
+        spotsList.push(spot.toJSON());
+    });
+
+    spotsList.forEach(spot => {
+        spot.SpotImages.forEach(image => {
+            if (image.preview === true) {
+                spot.previewImage = image.url;
+            }
+        })
+        if (!spot.previewImage) {
+            spot.previewImage = 'no preview image found'
+        }
+        delete spot.SpotImages;
+    })
+
+    spotsList.forEach(spot => {
+        totalStars = 0;
+        totalReviews = 0;
+        spot.Reviews.forEach(review => {
+            totalStars +=review.stars;
+            totalReviews++;
+        })
+        let avg = totalStars/totalReviews;
+        spot.avgRating = avg;
+        if (!avg) {
+            spot.avgRating = 'no ratings for this spot';
+
+        }
+        delete spot.Reviews;
+    })
+    // console.log(spots);
+    res.json(spotsList);
+})
+
+module.exports = router;
